739106dbeb26037d1e04414fe05308a1ee8489a1
Seveshy <danielalvesmajor@outlook.com>
2020-04-21 22:22:36 -0300

    Validando Credenciais

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import  appointmentsRouter from './appointments.routes';
 import  usersRouter from './user.routes';
+import  sessionRouter from './sessions.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
+routes.use('/sessions', sessionRouter); 
 routes.use('/users', usersRouter);
 
 
