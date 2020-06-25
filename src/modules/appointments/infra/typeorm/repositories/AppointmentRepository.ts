8e0192952e9a400cb4ddd702c529d045c78dfcb5
Daniel Major <danielalvesmajor@outlook.com>
2020-06-25 12:52:03 -0300

    Dependency Inversion Principle

--- src/modules/appointments/infra/typeorm/repositories/AppointmentRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentRepository.ts
@@ -1,20 +1,36 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { getRepository, Repository } from 'typeorm';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepositories';
 
 import Appointment from '../../typeorm/entities/Appointments';
 
-@EntityRepository(Appointment)
-class AppointmentRepository extends Repository<Appointment> 
-  
-  implements IAppointmentsRepository {
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+
+class AppointmentRepository  implements IAppointmentsRepository {
+  private ormRepository: Repository<Appointment>;
+
+  constructor() {
+    this.ormRepository = getRepository(Appointment);
+  }
+
   public async findByDate(date: Date): Promise<Appointment | undefined> {
 
-    const findAppointment = await this.findOne({
+    const findAppointment = await this.ormRepository.findOne({
       where: { date },
     });
 
     return findAppointment;
+  }
+
+  public async create({ 
+    provider_id, 
+    date
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = this.ormRepository.create({ provider_id, date });
+
+    await this.ormRepository.save(appointment)
+
+    return appointment;
   }
 }
 
--- src/modules/appointments/repositories/IAppointmentsRepositories.ts
+++ src/modules/appointments/repositories/IAppointmentsRepositories.ts
@@ -1,6 +1,9 @@
 import Appointment from '../infra/typeorm/entities/Appointments';
 
+import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
+
 export default interface IAppointmentsRepositories {
+    create(date: ICreateAppointmentDTO): Promise<Appointment>;
     findByDate(date: Date): Promise<Appointment | undefined>;
 
 }
