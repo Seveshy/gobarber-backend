397ba6dd725a883c6673e2297a8968a14632a3f9
Daniel Major <danielalvesmajor@outlook.com>
2020-07-06 22:00:41 -0300

    Refatorando módulo de usuário / 8:00

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -3,14 +3,14 @@ import { hash } from 'bcryptjs';
 import User from '../infra/typeorm/entities/User';
 import AppError from '@shared/errors/AppErrors';
 
-interface Request {
+interface IRequest {
     name: string;
     email: string;
     password: string;
 }
 
 class CreateUserService {
-    public async execute({ name, email, password }: Request): Promise<User> {
+    public async execute({ name, email, password }: IRequest): Promise<User> {
         const usersRepository = getRepository(User);
 
         const checkUserExists = await usersRepository.findOne({
