f9cd010dabb377de6eca6c24fec3e8944d5d7976
seveshy <danielalvesmajor@outlook.com>
2020-08-25 18:24:23 -0300

    Provider de storage

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -5,6 +5,7 @@ import { injectable, inject } from 'tsyringe';
 import uploadConfig from '@config/upload';
 import User from '../infra/typeorm/entities/User';
 
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
 import AppError from '@shared/errors/AppErrors';
 
@@ -17,7 +18,14 @@ interface IRequest {
 class UpdateUserAvatarService {
   constructor(
     @inject('UsersRepository')
-    private usersRepository: IUsersRepository) {}
+    private usersRepository: IUsersRepository,
+    
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider
+
+    )
+    
+    {}
 
   public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
     const user = await this.usersRepository.findById(user_id);
@@ -28,15 +36,12 @@ class UpdateUserAvatarService {
 
     if (user.avatar) {
 
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath); 
-    
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath);
-      }
+      await this.storageProvider.deleteFile(user.avatar);
     }
 
-    user.avatar = await avatarFilename;
+    const filename = await this.storageProvider.saveFile(avatarFilename);
+
+    user.avatar = await filename;
 
     await this.usersRepository.save(user);
   
