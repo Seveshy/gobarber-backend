d81a2745fc39ad7621ec0481b80f77337e493c4f
seveshy <danielalvesmajor@outlook.com>
2020-09-29 09:07:14 -0300

    Template de emails / 10m

---
+++ src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -0,0 +1,13 @@
+import { parse } from 'date-fns';
+import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
+import IMailTemplateProvider from '../models/IMailTemplateProvider';
+
+class FakeMailTemplateMailProvider implements IMailTemplateProvider {
+    public async parse({ template, 
+    }: 
+        IParseMailTemplateDTO): Promise<string> {
+        return template;
+    }
+}
+
+export default FakeMailTemplateMailProvider;
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -5,7 +5,21 @@ import DiskStorageProvider from './StorageProvider/implementations/DiskStoragePr
 
 import IMailProvider from './MailProvider/models/IMailProvider';
 
+import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
+import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+
 container.registerSingleton<IStorageProvider>(
     'StorageProvider',
     DiskStorageProvider,
 );
+
+container.registerSingleton<IMailTemplateProvider>(
+    'IMailTemplateProvider',
+    HandlebarsMailTemplateProvider,
+);
+
+
+// container.registerSingleton<IMailProvider>(
+//     'IMailProvider',
+//     new EtherealMailProvider,
+// );
