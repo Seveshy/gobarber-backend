62ce246510773fe578f29df81a8a36a0e36e7018
Seveshy <danielalvesmajor@outlook.com>
2020-11-11 15:34:43 -0300

    Listagem de prestadores

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import  appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
+import  providersRouter from '@modules/appointments/infra/http/routes/providers.routes';
 import  usersRouter from '@modules/users/infra/http/routes/user.routes';
 import  sessionRouter from '@modules/users/infra/http/routes/sessions.routes';
 import  passwordRoutes from '@modules/users/infra/http/routes/password.routes';
@@ -8,6 +9,7 @@ import  profileRouter from '@modules/users/infra/http/routes/profile.routes';
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
+routes.use('/providers', profileRouter);
 routes.use('/sessions', sessionRouter); 
 routes.use('/users', usersRouter);
 routes.use('/password', passwordRoutes);
