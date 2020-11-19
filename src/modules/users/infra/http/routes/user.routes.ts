d3b718ed1ceb3fb96734583f43576e7e1389a260
Seveshy <danielalvesmajor@outlook.com>
2020-11-19 13:43:44 -0300

    Validando dados / lib celebrate

--- src/modules/users/infra/http/routes/user.routes.ts
+++ src/modules/users/infra/http/routes/user.routes.ts
@@ -3,6 +3,7 @@ import {
 } from 'express';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
+import {celebrate, Segments, Joi } from 'celebrate';
 
 import UsersController from '../controllers/UsersController';
 import UserAvatarController from '../controllers/UserAvatarController';
@@ -14,7 +15,13 @@ const userAvatarController = new UserAvatarController();
 const usersRouter = Router();
 const upload = multer(uploadConfig);
 
-usersRouter.post('/', usersController.create)
+usersRouter.post('/', celebrate({
+  [ Segments.BODY ]: {
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }
+}), usersController.create)
 
 usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'),userAvatarController.update);
 
